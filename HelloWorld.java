2b96625e1eb077295dd4168df43ffb08fae22a32
Nathalie Spellerberg <nspeller@smail.uni-koeln.de>
2020-11-04 11:54:55 +0100

    Aufgabe 4
    Aenderungen an der HelloWorld Datei

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Anzahl der Jahre, die ich in Köln Studiere: 3");
-        System.out.println("Lazar Milosevic"); 
+        System.out.println("Anzahl der Jahre, die ich in Köln studiere: 3");
+        System.out.println("Lazar Milosevic und Nathalie Spellerberg");
+        System.out.println("Nathalie war hier"); 
     }
 }
