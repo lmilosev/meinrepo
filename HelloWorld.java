f00192568cf4c36e6f2bea2b08ab167c9e122b5b
Lazar Milosevic <lmilosev@smail.uni-koeln.de>
2020-11-04 11:24:36 +0100

    Anzahl der Jahre eingegeben

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Anzahl der Jahre, die ich in Köln Studiere: 3"); 
     }
 }
